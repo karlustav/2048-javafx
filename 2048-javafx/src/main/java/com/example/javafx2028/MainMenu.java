3a7d63d11fc1f208aa5993a2ad60f61ae84b3402
tugilane <marten.peljo@gmail.com>
2025-05-08 17:21:46 +0300

    peljo first commit vol 2

---
+++ 2048-javafx/src/main/java/com/example/javafx2028/MainMenu.java
@@ -0,0 +1,15 @@
+package com.example.javafx2028;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("oooo");
+    }
+}
